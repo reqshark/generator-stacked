ea088a3e8cd8df6efd104bb013fbe400e1f000ef
cl0udc0ntr0l <randylebeau@gmail.com>
2013-08-10 13:11:47 -0400

    "Index Complete"

--- app/index.js
+++ app/index.js
@@ -8,7 +8,19 @@ var StackedGenerator = module.exports = function StackedGenerator(args, options,
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
+    if(mocha){
+      this.log.write('\n\n\nYou are almost ready to start building your app. First you have to initialize your build...\n\n');
+      this.log.write('    > grunt init\n\n');
+      this.log.write("Then install Mocha globally (if you haven't already)\n\n");
+      this.log.write('    > npm install -g mocha\n\n');
+      this.log.write("Then you're done! Start up your server\n\n");
+      this.log.write("    > grunt server\n\n");
+    } else {
+      this.log.write('\n\n\nYou are almost ready to start building your app. First you have to initialize your build...\n\n');
+      this.log.write('    > grunt init\n\n');
+      this.log.write("Then you're done! Start up your server\n\n");
+      this.log.write("    > grunt server\n\n");
+    }
   });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
@@ -24,27 +36,155 @@ StackedGenerator.prototype.askFor = function askFor() {
 
   var prompts = [{
     type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
+    name: 'confirmed',
+    message: 'Hello! Welcome to Stacked. Ready to create?' ,
+    default: true
+  }, {
+    name: 'userName',
+    message: 'What is your name?',
+    default: 'A Person'
+  }, {
+    name: 'github',
+    message: 'What is your github username?',
+    default: 'someuser'
+  }, {
+    name: 'projectName',
+    message: 'What do you want to name your app?',
+    default: 'myApp'
+  }, {
+    name: 'initName',
+    message: 'What do you want to call your first MVC set?',
+    default: 'Index'
+  }, {
+    name: 'path',
+    message: 'What do you want to place your first MVC set? root -> public/js/app/[type]/',
+    default: ''
+  }, {
+    type: 'confirm',
+    name: 'less',
+    message: 'Do you want to use LESS?',
+    default: true
+  }, {
+    type: 'confirm',
+    name: 'mocha',
+    message: 'Do you want to use Mocha for server side testing?',
     default: true
   }];
 
   this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
+    this.confirmed = props.confirmed;
+    this.userName = props.userName;
+    this.initName = props.initName;
+    this.path = props.path;
+    this.less = props.less;
+    this.mocha = props.mocha;
+
+    this.path = this.path.replace(/\/?$/, '/');
 
     cb();
   }.bind(this));
 };
 
 StackedGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
+  
+  // Public Root
+  this.mkdir('public');
 
+  // CSS
+  this.mkdir('public/css');
+  this.mkdir('public/css/includes');
+  this.mkdir('public/css/includes/css');
+  this.mkdir('public/css/includes/less');
+
+  // IMG
+  this.mkdir('public/img');
+
+  // JS
+  this.mkdir('public/js');
+  this.mkdir('public/js/libs');
+  this.mkdir('public/js/tests');
+  this.mkdir('public/js/tests/config');
+  this.mkdir('public/js/tests/specs');
+
+  // APP
+  this.mkdir('public/js/app');
+  this.mkdir('public/js/app/collections');
+  this.mkdir('public/js/app/config');
+  this.mkdir('public/js/app/models');
+  this.mkdir('public/js/app/routers');
+  this.mkdir('public/js/app/templates');
+  this.mkdir('public/js/app/views');
+
+  // TESTS
+  this.mkdir('public/js/tests');
+  this.mkdir('public/js/tests/config');
+  this.mkdir('public/js/tests/specs');
+
+  // Server
+  this.mkdir('server');
+  this.mkdir('server/config');
+
+  // TL-FILES
   this.copy('_package.json', 'package.json');
   this.copy('_bower.json', 'bower.json');
+  this.copy('_.bowerrc', '.bowerrc');
+  this.copy('_Gruntfile.js', 'Gruntfile.js');
+  this.copy('_.gitignore', '.gitignore');
+  this.copy('_LISCENSE-MIT', 'LICENSE-MIT');
+  this.copy('_README.md', 'README.md');
+  this.copy('_travis.yml', '.travis.yml');
+
 };
 
 StackedGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
+  // Server
+  this.copy('server/_server.js', 'server/server.js');
+  this.copy('server/_API.js', 'server/API.js');
+  this.copy('server/_config.js', 'server/config/config.js');
+
+  // App
+  this.template('public/_Collection.js', 'public/js/app/collections/' + this.path + this.initName +'Collection.js');
+  this.template('public/_Model.js', 'public/js/app/models/' + this.path + this.initName +'Model.js');
+  this.template('public/_View.js', 'public/js/app/views/' + this.path + this.initName +'View.js');
+  this.template('public/_Template.html', 'public/js/app/templates/' + this.path + this.initName +'.html');
+  this.template('public/_Router.js', 'public/js/app/routers/Router.js');
+  this.template('public/_Init.js', 'public/js/app/config/Init.js');
+
+  // CSS
+  this.copy('public/_app.css', 'public/css/app.css');
+  this.copy('public/_jasmine.css', 'public/css/jasmine.css');
+  this.copy('public/_custom.css', 'public/css/includes/css/custom.css');
+
+  // Tests
+  this.copy('public/_TestInit.js', 'public/js/tests/config/TestInit.js');
+  this.copy('public/_spec.js', 'public/js/tests/specs/spec.js');
+  
 };
+
+StackedGenerator.prototype.less = function less() {
+  if(less){
+    this.mkdir('public/css/includes/less');
+    this.template('public/_custom.less', 'public/css/includes/less/custom.less');
+  }
+};
+
+StackedGenerator.prototype.mocha = function mocha() {
+  if(mocha){
+    this.mkdir('server/tests');
+    this.mkdir('server/tests/specs');
+    this.copy('server/tests/specs/spec.js');
+  }
+};
+
+StackedGenerator.prototype.getDependencies = function getDependencies() {
+  var cb = this.async();
+
+  this.installDependencies({
+      skipInstall: options['skip-install'],
+       callback: function(){
+        cb();
+    } 
+  });
+
+};
+
